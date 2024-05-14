ab71e7202f92e25a4083b04d447c228aed50a2d2
Rodolfo Barbeito <rbarbeito@gmail.com>
2024-05-14 11:16:24 +0200

    Add dot env to class connection

--- model/mysql_connection.py
+++ model/mysql_connection.py
@@ -1,3 +1,9 @@
+import mysql.connector
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 class Connection:
     __instance = None
 
@@ -6,7 +12,32 @@ class Connection:
             cls.__instance = super().__new__(cls)
             
         return cls.__instance
+    
     def __init__(self):
-        self.host = "localhost"
-        self.user = ""
-        self.passwd = ""
+        self.__host = os.getenv('MYSQL_HOST')
+        self.__user = os.getenv('MYSQL_USER')
+        self.__passwd = os.getenv('MYSQL_PASSWORD')
+        self.__db = os.getenv('MYSQL_DB')
+
+
+    def conexion_db(self):
+
+        try:
+            print("{}".format(self.__host))
+
+
+            return mysql.connector.connect(
+                host=self.__host,
+                user=self.__user,
+                password=self.__passwd,
+                database=self.__db)
+        
+        except Exception as e:
+            print("Error de conexion")
+            print(e)
+
+
+if __name__ == "__main__":
+
+    db = Connection()
+    db.conexion_db()
